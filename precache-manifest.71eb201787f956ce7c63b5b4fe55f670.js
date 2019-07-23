822d0c70d4de1407cd76a98aab85db0556bbdc85
Rizwan <rizwan@vrguest.com>
2019-07-23 16:30:06 +0500

    User login authentications

---
+++ precache-manifest.71eb201787f956ce7c63b5b4fe55f670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9be07d278e357d7039da",
+    "url": "/js/chunk-vendors.1e434d74.js"
+  },
+  {
+    "revision": "4d9d5c2db7293ff631de",
+    "url": "/js/app.d246fa09.js"
+  },
+  {
+    "revision": "55164d1e739a8f40176c5d71b0e8428b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9be07d278e357d7039da",
+    "url": "/css/chunk-vendors.ea12d630.css"
+  },
+  {
+    "revision": "4d9d5c2db7293ff631de",
+    "url": "/css/app.522baf64.css"
+  }
+];
